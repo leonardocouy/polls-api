e553482b8431e149d3380d5ee62cc94ddf1f2b23
Leonardo Flores <contato@leonardocouy.com>
2022-04-25 16:59:15 -0300

    Add logic to hash password before save

--- lib/polls/accounts/user.ex
+++ lib/polls/accounts/user.ex
@@ -2,10 +2,13 @@ defmodule Polls.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Inspect, except: [:password]}
   schema "users" do
     field :email, :string
     field :encrypted_password, :string
     field :name, :string
+
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -13,9 +16,18 @@ defmodule Polls.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :encrypted_password])
-    |> validate_required([:name, :email, :encrypted_password])
-    |> validate_format(:email, ~r/\A[^@\s]+@[^@\s]+\z/)
-    |> unique_constraint(:email, message: "Email already exists")
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> validate_length(:password, min: 6)
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> unsafe_validate_unique(:email, Polls.Repo)
+    |> unique_constraint(:email)
+    |> put_pass_hash()
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password, hash_key: :encrypted_password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
 end
