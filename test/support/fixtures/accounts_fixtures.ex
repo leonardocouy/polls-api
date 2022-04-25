6e5feb9485a94703d3cd5cd4dee5cd1186d5422e
Leonardo Flores <contato@leonardocouy.com>
2022-04-25 16:31:06 -0300

    Add initial Accounts context

---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,22 @@
+defmodule Polls.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Polls.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        encrypted_password: "some encrypted_password",
+        name: "some name"
+      })
+      |> Polls.Accounts.create_user()
+
+    user
+  end
+end
