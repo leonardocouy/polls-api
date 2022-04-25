fb71c6880ddd75c937acffae5700b48997a9a8fb
Leonardo Flores <contato@leonardocouy.com>
2022-04-25 16:14:32 -0300

    Change Repo module to read env vars

--- lib/polls/repo.ex
+++ lib/polls/repo.ex
@@ -2,4 +2,15 @@ defmodule Polls.Repo do
   use Ecto.Repo,
     otp_app: :polls,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+    config =
+      config
+      |> Keyword.put(:username, System.get_env("PGUSER") || "postgres")
+      |> Keyword.put(:password, System.get_env("PGPASSWORD") || "postgres")
+      |> Keyword.put(:hostname, System.get_env("PGHOST") || "localhost")
+      |> Keyword.put(:port, (System.get_env("PGPORT") || "5432") |> String.to_integer())
+
+    {:ok, config}
+  end
 end
