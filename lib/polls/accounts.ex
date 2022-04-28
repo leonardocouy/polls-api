4da30de64f994592db722c5061d84fb0a3aa3df6
Leonardo Flores <contato@leonardocouy.com>
2022-04-28 16:00:30 -0300

    Remove excessively doc from the project

--- lib/polls/accounts.ex
+++ lib/polls/accounts.ex
@@ -8,107 +8,30 @@ defmodule Polls.Accounts do
 
   alias Polls.Accounts.User
 
-  @doc """
-  Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
-  """
   def list_users do
     Repo.all(User)
   end
 
-  @doc """
-  Gets a single user by email
-
-  ## Examples
-
-      iex> get_user('test.com')
-      [%User{}, ...]
-
-  """
   def get_user_by_email(email), do: Repo.get_by(User, email: email)
 
-  @doc """
-  Gets a single user.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_user!(id), do: Repo.get!(User, id)
 
-  @doc """
-  Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a user.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_user(%User{} = user) do
     Repo.delete(user)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{data: %User{}}
-
-  """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
   end
@@ -117,16 +40,14 @@ defmodule Polls.Accounts do
   Returns if given email and password are valid
 
   ## Examples
+    iex> authenticate("email@email.com", "correct_password")
+    {:ok, %User{}}
 
+    iex> authenticate("inexistent_email@email.com", "correct_password")
+    {:error, {:not_found, "User not found"}}
 
-      iex> authenticate("email@email.com", "correct_password")
-      {:ok, %User{}}
-
-      iex> authenticate("inexistent_email@email.com", "correct_password")
-      {:error, {:not_found, "User not found"}}
-
-      iex> authenticate("email@email.com", "invalid_password")
-      {:error, :unauthorized}
+    iex> authenticate("email@email.com", "invalid_password")
+    {:error, :unauthorized}
   """
   def authenticate_user(email, password) do
     case get_user_by_email(email) do
--- lib/polls/core.ex
+++ lib/polls/core.ex
@@ -8,48 +8,13 @@ defmodule Polls.Core do
 
   alias Polls.Core.Poll
 
-  @doc """
-  Returns the list of polls.
-
-  ## Examples
-
-      iex> list_polls()
-      [%Poll{}, ...]
-
-  """
   def list_polls do
     Repo.all(Poll)
     |> Repo.preload([:owner, :options])
   end
 
-  @doc """
-  Gets a single poll.
-
-  Raises `Ecto.NoResultsError` if the Poll does not exist.
-
-  ## Examples
-
-      iex> get_poll!(123)
-      %Poll{}
-
-      iex> get_poll!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_poll!(id), do: Repo.get!(Poll, id) |> Repo.preload([:owner, :options])
 
-  @doc """
-  Creates a poll.
-
-  ## Examples
-
-      iex> create_poll(%{field: value})
-      {:ok, %Poll{}}
-
-      iex> create_poll(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_poll(attrs \\ %{}) do
     %Poll{}
     |> Poll.changeset(attrs)
@@ -60,145 +25,44 @@ defmodule Polls.Core do
     end
   end
 
-  @doc """
-  Updates a poll.
-
-  ## Examples
-
-      iex> update_poll(poll, %{field: new_value})
-      {:ok, %Poll{}}
-
-      iex> update_poll(poll, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_poll(%Poll{} = poll, attrs) do
     poll
     |> Poll.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a poll.
-
-  ## Examples
-
-      iex> delete_poll(poll)
-      {:ok, %Poll{}}
-
-      iex> delete_poll(poll)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_poll(%Poll{} = poll) do
     Repo.delete(poll)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking poll changes.
-
-  ## Examples
-
-      iex> change_poll(poll)
-      %Ecto.Changeset{data: %Poll{}}
-
-  """
   def change_poll(%Poll{} = poll, attrs \\ %{}) do
     Poll.changeset(poll, attrs)
   end
 
   alias Polls.Core.Option
 
-  @doc """
-  Returns the list of options.
-
-  ## Examples
-
-      iex> list_options()
-      [%Option{}, ...]
-
-  """
   def list_options do
     Repo.all(Option)
   end
 
-  @doc """
-  Gets a single option.
-
-  Raises `Ecto.NoResultsError` if the Option does not exist.
-
-  ## Examples
-
-      iex> get_option!(123)
-      %Option{}
-
-      iex> get_option!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_option!(id), do: Repo.get!(Option, id)
 
-  @doc """
-  Creates a option.
-
-  ## Examples
-
-      iex> create_option(%{field: value})
-      {:ok, %Option{}}
-
-      iex> create_option(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_option(attrs \\ %{}) do
     %Option{}
     |> Option.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a option.
-
-  ## Examples
-
-      iex> update_option(option, %{field: new_value})
-      {:ok, %Option{}}
-
-      iex> update_option(option, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_option(%Option{} = option, attrs) do
     option
     |> Option.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a option.
-
-  ## Examples
-
-      iex> delete_option(option)
-      {:ok, %Option{}}
-
-      iex> delete_option(option)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_option(%Option{} = option) do
     Repo.delete(option)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking option changes.
-
-  ## Examples
-
-      iex> change_option(option)
-      %Ecto.Changeset{data: %Option{}}
-
-  """
   def change_option(%Option{} = option, attrs \\ %{}) do
     Option.changeset(option, attrs)
   end
